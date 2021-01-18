6940fa66b85a2d38ee136c90fc6c95be16d9a47e
René Payán Téllez <payantellez1g@hotmail.com>
2021-01-18 03:36:47 -0600

    Imprime datos

--- capturador.cpp
+++ capturador.cpp
@@ -116,7 +116,7 @@ std::string Capturador::toString(){
     readlink(path, result, sizeof(result)-1);
     tmp="Archivo de salida: ";
     retorno+=tmp+result+'\n';
-    retorno+="Nivel de verbosidad: "+std::to_string(this->nivelVerbosidad); 
+    retorno+="Nivel de verbosidad: "+std::to_string(this->nivelVerbosidad)+'\n'; 
     retorno+="Red: "+this->obtenerRed()+'\n';
     retorno+="Mascara de red: "+this->obtenerMascaraDeRed();
 	return retorno;
--- filtro.cpp
+++ filtro.cpp
@@ -30,5 +30,11 @@ void Filtro::setIpDestino(std::string ipDestino){
 }
 
 std::string Filtro::toString(){
-    
+    std::string retorno = "";
+    retorno+="Aplicacion: "+to_string(aplicacion)+'\n';
+    retorno+="Puerto de origen: "+to_string(puertoOrigen)+'\n';
+    retorno+="Puerto de destino: "+to_string(puertoDestino)+'\n';
+    retorno+="Ip de Origen: "+ipOrigen+'\n';
+    retorno+="Ip de Destino: "+ipDestino+'\n';
+    return retorno;
 }
