ef91f84758b0b712c884dca6bd3129090e7f391d
Ares Ulises Juárez Martínez <aresulises8@hotmail.com>
2021-01-18 05:59:47 -0600

    Agregados los metodos tojson a cada paquete

--- paqueteTCP.hpp
+++ paqueteTCP.hpp
@@ -3,6 +3,8 @@
 
 #include <string>
 #include "paquete.hpp"
+#include "json.hpp"
+using json = nlohmann::json;
 class PaqueteTCP : public Paquete
 {
 private:
@@ -18,5 +20,18 @@ private:
 public:
     PaqueteTCP(int numeroDeSecuencia, int tamanioVentana, int numeroACK, std::string offsetDeDatos, std::string options, std::string banderas, std::string res, std::string punteroUrgente, long long int idCaptura, int tipo, std::string datosRAW, int aplicacion, time_t fechaCaptura, std::string ipOrigen, std::string ipDestino, int puertoOrigen, int puertoDestino, std::string checksum) : Paquete(idCaptura, tipo, datosRAW, aplicacion, fechaCaptura, ipOrigen, ipDestino, puertoOrigen, puertoDestino, checksum), numeroDeSecuencia(numeroDeSecuencia), tamanioVentana(tamanioVentana), numeroACK(numeroACK), offsetDeDatos(offsetDeDatos), options(options), banderas(banderas), res(res), punteroUrgente(punteroUrgente) {}
     PaqueteTCP(int numeroDeSecuencia, int tamanioVentana, int numeroACK, std::string offsetDeDatos, std::string options, std::string banderas, std::string res, std::string punteroUrgente, Paquete base) : Paquete(base), numeroDeSecuencia(numeroDeSecuencia), tamanioVentana(tamanioVentana), numeroACK(numeroACK), offsetDeDatos(offsetDeDatos), options(options), banderas(banderas), res(res), punteroUrgente(punteroUrgente) {}
+    std::string toJson()
+    {
+        json output = json::parse(Paquete::toJson());
+        output["numeroDeSecuencia"] = numeroDeSecuencia;
+        output["tamanioVentana"] = tamanioVentana;
+        output["numeroACK"] = numeroACK;
+        output["offsetDeDatos"] = offsetDeDatos;
+        output["options"] = options;
+        output["banderas"] = banderas;
+        output["res"] = res;
+        output["punteroUrgente"] = punteroUrgente;
+        return output.dump();
+    }
 };
 #endif
--- paqueteUDP.hpp
+++ paqueteUDP.hpp
@@ -3,6 +3,8 @@
 
 #include <string>
 #include "paquete.hpp"
+#include "json.hpp"
+using json = nlohmann::json;
 class PaqueteUDP : public Paquete
 {
 private:
@@ -11,5 +13,12 @@ private:
 public:
     PaqueteUDP(int tamanio, long long int idCaptura, int tipo, std::string datosRAW, int aplicacion, time_t fechaCaptura, std::string ipOrigen, std::string ipDestino, int puertoOrigen, int puertoDestino, std::string checksum) : Paquete(idCaptura, tipo, datosRAW, aplicacion, fechaCaptura, ipOrigen, ipDestino, puertoOrigen, puertoDestino, checksum), tamanio(tamanio) {}
     PaqueteUDP(int tamanio, Paquete base) : Paquete(base), tamanio(tamanio) {}
+
+    std::string toJson()
+    {
+        json output = json::parse(Paquete::toJson());
+        output["tamanio"] = tamanio;
+        return output.dump();
+    }
 };
 #endif
