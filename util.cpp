81d7d678e20a3d27927944746d0bf58d7a915091
René Payán Téllez <payantellez1g@hotmail.com>
2021-01-18 04:54:22 -0600

    Imprime datos

--- util.cpp
+++ util.cpp
@@ -7,7 +7,7 @@ std::string Util::intToHexString(int valor){
     std::stringstream stream;
     stream << std::hex << valor;
     std::string retorno = stream.str();
-    if(retorno.length()<0){
+    if(retorno.length()==1){
         retorno = '0'+retorno;
     }
     return retorno;
