09873410591197a1156b7147db03387dae84f81f
René Payán Téllez <payantellez1g@hotmail.com>
2021-01-18 03:46:25 -0600

    Imprime datos

--- capturador.cpp
+++ capturador.cpp
@@ -16,11 +16,11 @@ Capturador::Capturador(pcap_if_t *interface, std::vector<Filtro>filtros, FILE *a
 bool Capturador::ValidarFiltros(){
 	return true;
 }
-void Capturador::print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header) {    
+static void Capturador::print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header) {    
     printf("Packet capture length: %d\n", packet_header.caplen);
     printf("Packet total length %d\n", packet_header.len);
 }
-void my_packet_handler(u_char *args,const struct pcap_pkthdr *packet_header,const u_char *packet_body){
+static void Capturador::my_packet_handler(u_char *args,const struct pcap_pkthdr *packet_header,const u_char *packet_body){
     print_packet_info(packet_body, *packet_header);
     return;
 }
--- capturador.hpp
+++ capturador.hpp
@@ -19,8 +19,8 @@ class Capturador{
         void iniciarCaptura();
         void detenerCaptura();
         std::string toString();     
-        void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header);   
-        //void my_packet_handler(u_char *args,const struct pcap_pkthdr *packet_header,const u_char *packet_body);        
+        static void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header);   
+        static void my_packet_handler(u_char *args,const struct pcap_pkthdr *packet_header,const u_char *packet_body);        
         void imprimirRed();
         std::string obtenerRed();
         std::string obtenerMascaraDeRed();
