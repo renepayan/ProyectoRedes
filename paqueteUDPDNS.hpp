0ebac62b5152240c14ba30e0f06892a70aec209d
Ares Ulises Juárez Martínez <aresulises8@hotmail.com>
2021-01-18 05:07:19 -0600

    Hechos las herencias

--- paqueteUDPDNS.hpp
+++ paqueteUDPDNS.hpp
@@ -3,18 +3,20 @@
 
 #include <string>
 #include "paqueteUDP.hpp"
-class PaqueteUDPDNS: public PaqueteUDP{
-    private:
-        int identificacion;
-        int totalPreguntas;
-        int totalRespuestas;
-        int opCodigo;
-        int rcCodigo;
-        int banderas;
-        int registrosAutorizacionTotal;
-        int recursosAutorizacionTotal;
-    public:
-        PaqueteUDPDNS(int tamanio, long long int idCaptura, int tipo, std::string datosRAW, int aplicacion, time_t fechaCaptura, std::string ipOrigen, std::string ipDestino, int puertoOrigen, int puertoDestino, std::string checksum):PaqueteUDP(tamanio,idCaptura,tipo,datosRAW,aplicacion,fechaCaptura,ipOrigen,ipDestino,puertoOrigen,puertoDestino,checksum){}
-        PaqueteUDPDNS(int tamanio, PaqueteUDP base):PaqueteUDP(base){}         
+class PaqueteUDPDNS : public PaqueteUDP
+{
+private:
+    int identificacion;
+    int totalPreguntas;
+    int totalRespuestas;
+    int opCodigo;
+    int rcCodigo;
+    int banderas;
+    int registrosAutorizacionTotal;
+    int recursosAutorizacionTotal;
+
+public:
+    PaqueteUDPDNS(int identificacion, int totalPreguntas, int totalRespuestas, int opCodigo, int rcCodigo, int banderas, int registrosAutorizacionTotal, int recursosAutorizacionTotal, int tamanio, long long int idCaptura, int tipo, std::string datosRAW, int aplicacion, time_t fechaCaptura, std::string ipOrigen, std::string ipDestino, int puertoOrigen, int puertoDestino, std::string checksum) : PaqueteUDP(tamanio, idCaptura, tipo, datosRAW, aplicacion, fechaCaptura, ipOrigen, ipDestino, puertoOrigen, puertoDestino, checksum), identificacion(identificacion), totalPreguntas(totalPreguntas), totalRespuestas(totalRespuestas), opCodigo(opCodigo), rcCodigo(rcCodigo), banderas(banderas), registrosAutorizacionTotal(registrosAutorizacionTotal), recursosAutorizacionTotal(recursosAutorizacionTotal) {}
+    PaqueteUDPDNS(int identificacion, int totalPreguntas, int totalRespuestas, int opCodigo, int rcCodigo, int banderas, int registrosAutorizacionTotal, PaqueteUDP base) : PaqueteUDP(base), identificacion(identificacion), totalPreguntas(totalPreguntas), totalRespuestas(totalRespuestas), opCodigo(opCodigo), rcCodigo(rcCodigo), banderas(banderas), registrosAutorizacionTotal(registrosAutorizacionTotal), recursosAutorizacionTotal(recursosAutorizacionTotal) {}
 };
 #endif
